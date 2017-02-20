f6b252dd98632341ff15ccf58d120057250bd15d
RandyRomero <a.ya.miheev@gmail.com>
2017-02-20 18:33:33 +0300

    make working file with description

---
+++ zipByExtention.py
@@ -0,0 +1,5 @@
+# !python3 
+
+'''Program that ask user which type of files he wants to archive (by extention), ask for path and go through this path looking for every file with these extentions. Also user should denote if he wants to archive files with these extentions of all other files except files with these extentions'''
+
+import os, shutil, 
